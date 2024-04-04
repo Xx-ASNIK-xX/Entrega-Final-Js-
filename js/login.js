1c5a49d7de47f063981c5c057bf84814ff500203
Nicolas Sassone <nico_distortion@hotmail.com>
2024-04-04 07:54:30 -0300

    ultimos cambios

--- js/login.js
+++ js/login.js
@@ -28,7 +28,7 @@ function validarLogin(event) {
                 // Mostrar alerta de intentos fallidos
                 Swal.fire('Inicio de sesión incorrecto. Ha superado el número máximo de intentos.').then(() => {
                     // Redirigir a otra página después de superar el número máximo de intentos
-                    window.location.href = '../index.html';
+                    window.location.href = '../pages/registro.html';
                 });
             } else {
                 // Mostrar alerta de nombre de usuario o contraseña incorrectos
@@ -39,7 +39,7 @@ function validarLogin(event) {
         // Mostrar alerta de usuario no registrado
         Swal.fire('No hay ningún usuario registrado. Por favor, regístrate primero.').then(() => {
             // Redirigir a la página de registro si no hay usuarios registrados
-            window.location.href = '../index.html';
+            window.location.href = '../pages/registro.html';
         });
     }
 
